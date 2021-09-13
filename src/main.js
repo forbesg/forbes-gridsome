244b9bdb22cebccc3af8b99545bfff4fddcccf93
Forbes Gray <fnbg75@gmail.com>
2021-09-13 21:17:14 +0100

    Remove Content Security header & fix card image width and height to disolay the correct aspect ratio

--- src/main.js
+++ src/main.js
@@ -1,72 +1,73 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/scss/styles.scss'
+import DefaultLayout from "~/layouts/Default.vue";
+import "~/assets/scss/styles.scss";
 
-const hostname = process.env.GRIDSOME_HOSTNAME
+const hostname = process.env.GRIDSOME_HOSTNAME;
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
 
   /* Add Head Meta Tags */
   head.meta.push({
-    key: 'description',
-    name: 'description',
-    content: 'Forbes Gray is a Freelance Web Developer in Edinburgh, specialising in fast, responsive, SEO friendly websites and web applications'
-  })
+    key: "description",
+    name: "description",
+    content:
+      "Forbes Gray is a Freelance Web Developer in Edinburgh, specialising in fast, responsive, SEO friendly websites and web applications"
+  });
 
   head.meta.push({
-    key: 'author',
-    name: 'author',
-    content: 'Forbes Gray <fnbg75@gmail.com>'
-  })
+    key: "author",
+    name: "author",
+    content: "Forbes Gray <fnbg75@gmail.com>"
+  });
 
   head.link.push({
-    key: 'canonical',
-    rel: 'canonical',
+    key: "canonical",
+    rel: "canonical",
     href: hostname
-  })
+  });
 
   head.link.push({
-    rel: 'apple-touch-icon',
-    sizes: '108x180',
-    href: '/apple-touch-icon.png'
-  })
+    rel: "apple-touch-icon",
+    sizes: "108x180",
+    href: "/apple-touch-icon.png"
+  });
 
   head.link.push({
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '32x32',
-    href: '/favicon-32x32.png'
-  })
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png"
+  });
 
   head.link.push({
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '16x16',
-    href: '/favicon-16x16.png'
-  })
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png"
+  });
 
   head.link.push({
-    rel: 'manifest',
-    href: '/site.webmanifest'
-  })
+    rel: "manifest",
+    href: "/site.webmanifest"
+  });
 
   head.link.push({
-    rel: 'mask-icon',
-    href: '/safari-pinned-tab.svg',
-    color: '#5bbad5'
-  })
+    rel: "mask-icon",
+    href: "/safari-pinned-tab.svg",
+    color: "#5bbad5"
+  });
 
   head.meta.push({
-    name: 'msapplication-TileColor',
-    content: '#00aba9'
-  })
+    name: "msapplication-TileColor",
+    content: "#00aba9"
+  });
 
   head.meta.push({
-    name: 'theme-color',
-    content: '#083749'
-  })
+    name: "theme-color",
+    content: "#083749"
+  });
 }
